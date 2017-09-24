a49c3428ca7c423c9c8bf594f5211ad85235ef05
yshuman1 <yshuman@loyalloops.com>
2017-09-24 14:43:05 -0700

    revert app.listen

--- app.js
+++ app.js
@@ -74,6 +74,6 @@ app.get('/:encoded_id', function(req, res){
 
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
-  console.log("Listening"); 
+var server = app.listen(3000, function () {
+  console.log('Server listening on port 3000');
 });
