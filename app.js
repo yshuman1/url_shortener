aef6af3299f7f66bbb051df956433b24298bb60e
yshuman1 <yshuman@loyalloops.com>
2017-09-30 17:07:18 -0700

    added reminder about http

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.get('/:encoded_id', function(req, res){
     Url.findOne({_id: id}, function (err, doc){
       if (doc) {
         
-        res.redirect(doc.long_url);
+        res.redirect(301, doc.long_url);
         console.log(doc.long_url);
       } else {
         res.redirect(config.webhost);
